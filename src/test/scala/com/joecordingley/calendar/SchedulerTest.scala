07dfb186c294442b6b450addc34d6a37b552dc96
JoeCordingley <joewcordingley@gmail.com>
2017-09-19 22:38:59 +0100

    added test for futures and scheduler

---
+++ src/test/scala/com/joecordingley/calendar/SchedulerTest.scala
@@ -0,0 +1,8 @@
+package com.joecordingley.calendar
+
+/**
+  * Created by joe on 19/09/17.
+  */
+class SchedulerTEst {
+
+}
---
+++ src/test/scala/com/joecordingley/calendar/StreamsTry.scala
@@ -0,0 +1,8 @@
+package com.joecordingley.calendar
+
+/**
+  * Created by joe on 15/09/17.
+  */
+object StreamsTry {
+
+}
